0319ac67a28643c9b85f9b94b7e8d33f0c4990ed
Alexandre Pereira <naitwa.alexandre@gmail.com>
2021-03-01 06:49:09 -0300

    Draw img method

--- script.js
+++ script.js
@@ -218,6 +218,12 @@ function handleFloatingMessages() {
 
 
 //enemies
+const enemyTypes = [];
+const enemy1 = new Image();
+enemy1.src = 'sprites/enemy1.png';
+enemyTypes.push(enemy1);
+
+
 class Enemy {
     constructor(verticalPosition){
         this.x = canvas.width;
@@ -228,9 +234,20 @@ class Enemy {
         this.movement = this.speed;
         this.health = 100;
         this.maxHealth = this.health;
+        this.enemyType = enemyTypes[0];
+        this.frameX = 0;
+        this.frameY = 0;
+        this.minFrame = 0;
+        this.maxFrame = 4;
+        this.spriteWidth = 256;
+        this.spriteHeight = 256;
     }
     update(){
         this.x -= this.movement;
+        if (frame % 10 === 0 ) {
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = this.minFrame;
+        }
     }
     draw(){
         ctx.fillStyle = 'red';
@@ -238,6 +255,8 @@ class Enemy {
         ctx.fillStyle = 'black';
         ctx.font = '30px Orbitron';
         ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+        //ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh);
+        ctx.drawImage(this.enemyType, this.frameX * this.spriteWidth, 0,  this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 function handleEnemies(){
